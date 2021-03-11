68779a8ba9fd38b927f9bccbcac210d9349c136d
Yuta Saito <kateinoigakukun@gmail.com>
2021-03-11 15:40:53 +0900

    bump 0.2.0

--- mull-xctest.rb
+++ mull-xctest.rb
@@ -1,8 +1,8 @@
 class MullXctest < Formula
-  version "0.1.2"
+  version "0.2.0"
   desc "Experimental mutation testing tool for Swift and XCTest powered by mull"
   homepage "https://github.com/kateinoigakukun/mull-xctest"
-  url "https://github.com/kateinoigakukun/mull-xctest/releases/download/0.1.2/mull-xctest-x86_64-apple-darwin.tar.gz"
+  url "https://github.com/kateinoigakukun/mull-xctest/releases/download/0.2.0/mull-xctest-x86_64-apple-darwin.tar.gz"
   sha256 "381a4eb3570e9238b7d6f167c7ce7cb243754c63bb6cbd7afa71122ce7bc874c"
 
   def install
