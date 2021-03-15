ef48adbeb589aabc5c6cc326be150e24116c0190
Yuta Saito <kateinoigakukun@gmail.com>
2021-03-15 10:30:53 +0900

    Bump 0.3.0

--- mull-xctest.rb
+++ mull-xctest.rb
@@ -1,9 +1,9 @@
 class MullXctest < Formula
-  version "0.2.0"
+  version "0.3.0"
   desc "Experimental mutation testing tool for Swift and XCTest powered by mull"
   homepage "https://github.com/kateinoigakukun/mull-xctest"
-  url "https://github.com/kateinoigakukun/mull-xctest/releases/download/0.2.0/mull-xctest-x86_64-apple-darwin.tar.gz"
-  sha256 "5014a8c4e267ef15d4126bb5a377e1cfb5647edb1d670950c8b96680b4f94140"
+  url "https://github.com/kateinoigakukun/mull-xctest/releases/download/0.3.0/mull-xctest-x86_64-apple-darwin.tar.gz"
+  sha256 "1f7a3c41808b7d2e04269154c901688be01e6fb014c2d842b720ad12b6a9d89b"
 
   def install
     prefix.install Dir["local/*"]
